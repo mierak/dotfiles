90ee10b25ce72269a09bfb4280378b26875e00d6
MieraK <mrkpwnz@gmail.com>
2022-09-21 21:36:26 +0200

    remove awesomewm config

--- .config/awesome/snippets/window_swallow.lua
+++
@@ -1,7 +0,0 @@
-local beautiful = require("beautiful")
-
-beautiful.parent_filter_list = { "Vivaldi-stable", "code-oss", "code", "discord", "firefox", "Microsoft Teams - Preview" }
-beautiful.dont_swallow_filter_activated  = true   
-
-local bling = require("bling")
-bling.module.window_swallowing.start()
