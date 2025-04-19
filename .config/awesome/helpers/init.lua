782d1b5c7d2785cb6f86d3aa53e13d8c045093a4
mrk <mrkpwnz@gmail.com>
2025-04-19 19:40:08 +0200

    remove awesome

--- .config/awesome/helpers/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-    misc   = require("helpers.misc"),
-    table  = require("helpers.table"),
-    string = require("helpers.string"),
-    run    = require("helpers.run"),
-}
--- .config/awesome/modules/bar_widgets/init.lua
+++
@@ -1,5 +0,0 @@
-local current_module = ...
-
-return function (args)
-    return require(current_module .. "." .. args.style):new(args)
-end
