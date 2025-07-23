3739cdbe3b26c5bf00bbe3f3059e81cb24c84494
mrk <mrkpwnz@gmail.com>
2025-07-23 13:35:44 +0200

    Update .config/nvim/lua/mrk/set.lua

--- dot_config/nvim/lua/mrk/set.lua
+++ dot_config/nvim/lua/mrk/set.lua
@@ -22,3 +22,22 @@ vim.o.splitright = true
 
 vim.o.smartcase = true
 vim.o.ignorecase = true
+
+if vim.env.TMUX ~= nil then
+	local copy = { "tmux", "load-buffer", "-w", "-" }
+	local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+	vim.g.clipboard = {
+		name = "tmux",
+		copy = {
+			["+"] = copy,
+			["*"] = copy,
+		},
+		paste = {
+			["+"] = paste,
+			["*"] = paste,
+		},
+		cache_enabled = 0,
+	}
+else
+	vim.g.clipboard = "osc52"
+end
